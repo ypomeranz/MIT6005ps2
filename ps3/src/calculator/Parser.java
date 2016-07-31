bd5663539e9568b5342511b42db0d8e7f40d9698
yoni <yoni.pomeranz@hotmail.com>
2016-07-31 22:56:51 +0000

    some version of the parser done - some problems known and more testing needed

--- ps3/src/calculator/Parser.java
+++ ps3/src/calculator/Parser.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import calculator.Lexer;
+import calculator.Lexer.Token;
+import java.util.ArrayList;
 
 /*
  * TODO define your grammar from problem 1 here
@@ -64,14 +66,287 @@ class Parser {
 
 	private static final double PT_PER_IN = 72;
 	private final Lexer lexer;
+	public ArrayList<Token> alltokens;
 
 	// TODO write method spec
+	/* input a lexer, which will give you tokens based on next 
+	*/ 
 	Parser(Lexer lexer) {
-		// TODO implement for Problem 3
+		this.lexer = lexer;
+		ArrayList<Token> fulltokenlist = new ArrayList<Token>();
+		Token x = lexer.next();
+			while (x.type != Type.EOF){
+				fulltokenlist.add(x);
+				x = lexer.next();
+			}
+		alltokens=fulltokenlist;
 	}
 
 	// TODO write method spec
-	public Value evaluate() {
-		// TODO implement for Problem 3
+	// call with an expression to evaluate - keep recursively calling until you have an answer
+	// note to self trying to adjust to have code work by mutating the array - beware of code from the older method
+	public Value evaluate(ArrayList<Token> tokens) {
+		int stage = 0;
+		String operator = new String();
+		Value firstvalue=new Value(0, ValueType.SCALAR);
+		Value secondvalue=new Value(0, ValueType.SCALAR);
+		Value answer = new Value(0, ValueType.SCALAR);
+		while (tokens.size()>0 && stage < 3){
+			stage++;
+			
+			if (stage == 1){
+				//evaluate the first part of the expression
+				
+				if (tokens.get(0).type == Type.NUMBER){
+					firstvalue = evalNumber(tokens);
+					
+				} else if (tokens.get(0).type == Type.OPENPAREN){
+					firstvalue = evalExpression(tokens);
+					//expression may be followed by a unit type to convert to
+					if (tokens.size()>0){
+						if (tokens.get(0).type==Type.UNIT && firstvalue.type==ValueType.SCALAR){
+							firstvalue = convertScalar(firstvalue, tokens.get(0).text);
+							tokens.remove(0);
+						} else if (tokens.get(0).type==Type.UNIT && firstvalue.type!=ValueType.SCALAR){
+							firstvalue = convertNonScalar(firstvalue, tokens.get(0).text);
+							tokens.remove(0);
+						}
+					}
+				} else {
+					//illegal expression - raise error - until I implement exceptions, working with a print statement
+					System.out.println("Error! First part of expression invalid!");
+				}
+			} else if (stage == 2){
+				if (tokens.get(0).type==Type.OPERATOR){
+					//save the operator
+					operator = tokens.get(0).text;
+					tokens.remove(0);
+				} else {
+					//invalid expression
+					System.out.println("Problem with the Syntax! Expected operator!");
+				}
+			} else if (stage == 3){
+				//evaluate the second part of the expression
+				if (tokens.get(0).type == Type.NUMBER){
+					secondvalue = evalNumber(tokens);
+					
+				} else if (tokens.get(0).type == Type.OPENPAREN){
+					secondvalue = evalExpression(tokens);
+					//expression may be followed by a unit type to convert to
+					if (tokens.size()>0){
+						if (tokens.get(0).type==Type.UNIT && secondvalue.type==ValueType.SCALAR){
+							secondvalue = convertScalar(secondvalue, tokens.get(0).text);
+							tokens.remove(0);
+						} else if (tokens.get(0).type==Type.UNIT && secondvalue.type != ValueType.SCALAR){
+							secondvalue = convertNonScalar(secondvalue, tokens.get(0).text);
+							tokens.remove(0);
+						}
+					}
+				} else {
+					//illegal expression - raise error - until I implement exceptions, working with a print statement
+					System.out.println("Error getting second part of expression!");
+				}
+			} 
+
+			
+		//if we get to this point, raise an error - we ran through the whole list, but didn't find a complete expression	
+		//i wrote a return statement because java wanted it - hope this isn't crazy
+		//throw new RuntimeException("Error - failed to compute");
+		
+		}
+		//evaluate the expression and return a Value
+		//four cases - one for each operation
+		if (operator.charAt(0) == '+'){
+			double answervalue = firstvalue.value + secondvalue.value;
+			if (firstvalue.type ==  ValueType.SCALAR && secondvalue.type == ValueType.SCALAR){
+				answer = new Value(answervalue, ValueType.SCALAR);
+			} else if ((firstvalue.type == ValueType.INCHES | secondvalue.type == ValueType.INCHES) 
+							&& (firstvalue.type!=ValueType.POINTS && secondvalue.type!=ValueType.POINTS)){
+				answer = new Value(answervalue, ValueType.INCHES);	
+			} else if ((firstvalue.type==ValueType.POINTS | secondvalue.type == ValueType.POINTS) 
+							&& firstvalue.type != ValueType.INCHES && secondvalue.type != ValueType.INCHES){
+				answer = new Value(answervalue, ValueType.POINTS);
+			} else if ((firstvalue.type==ValueType.POINTS && secondvalue.type==ValueType.INCHES) 
+							| (firstvalue.type==ValueType.INCHES && secondvalue.type==ValueType.POINTS)){
+				answer = new Value(answervalue, firstvalue.type);	
+			}
+			
+		} else if (operator.charAt(0) == '-'){
+			double answervalue = firstvalue.value - secondvalue.value;
+			if (firstvalue.type ==  ValueType.SCALAR && secondvalue.type == ValueType.SCALAR){
+				answer = new Value(answervalue, ValueType.SCALAR);
+			} else if ((firstvalue.type == ValueType.INCHES | secondvalue.type == ValueType.INCHES) 
+							&& firstvalue.type!=ValueType.POINTS && secondvalue.type!=ValueType.POINTS){
+				answer = new Value(answervalue, ValueType.INCHES);	
+			} else if ((firstvalue.type==ValueType.POINTS | secondvalue.type == ValueType.POINTS) 
+							&& firstvalue.type != ValueType.INCHES && secondvalue.type != ValueType.INCHES){
+				answer = new Value(answervalue, ValueType.POINTS);
+			} else if ((firstvalue.type==ValueType.POINTS && secondvalue.type==ValueType.INCHES) 
+							| (firstvalue.type==ValueType.INCHES && secondvalue.type==ValueType.POINTS)){
+				answer = new Value(answervalue, firstvalue.type);	
+			}
+			
+			
+		} else if (operator.charAt(0) == '*'){
+			double answervalue = firstvalue.value * secondvalue.value;
+			//if one of the values is a scalar, return in the format of the other (even if scalar)
+			//otherwise return in terms of the first value
+			if (firstvalue.type==ValueType.SCALAR){
+				answer = new Value(answervalue, secondvalue.type);
+			} else if (secondvalue.type == ValueType.SCALAR){
+				answer = new Value(answervalue, firstvalue.type);
+			} else{
+				answer = new Value(answervalue, firstvalue.type);
+			}
+			
+		} else if (operator.charAt(0) == '/'){
+			if (secondvalue.value == (double)0.0){
+				//raise a divide by zero error
+				//System.out.println("Divide by zero error!");
+				throw new RuntimeException("Divide by zero error");
+			}
+			
+			double answervalue = firstvalue.value / secondvalue.value;
+			
+			
+			if ((firstvalue.type == secondvalue.type) | (firstvalue.type!=ValueType.SCALAR 
+							&& secondvalue.type != ValueType.SCALAR)){
+				answer = new Value(answervalue, ValueType.SCALAR);
+			} else if (firstvalue.type != ValueType.SCALAR){
+				answer = new Value(answervalue, firstvalue.type);
+			} else if (secondvalue.type != ValueType.SCALAR){
+				answer = new Value(answervalue, secondvalue.type);
+			}
+			
+		}
+		return answer;
+		
+	}
+	
+	
+	public Value evalNumber(ArrayList<Token> tokens){
+		double num = Double.parseDouble(tokens.get(0).text);
+		tokens.remove(0);
+		Value numvalue = new Value(num, ValueType.SCALAR);
+		if (tokens.size() > 0){
+			//check to see if there are units
+			if (tokens.get(0).type == Type.UNIT){
+				//check to see if the units are in or pt and save first value
+				if (tokens.get(0).text == "in"){
+					tokens.remove(0);
+					numvalue = new Value(PT_PER_IN * num, ValueType.INCHES);
+				} else if (tokens.get(0).text=="pt"){
+					tokens.remove(0);
+					numvalue = new Value(num, ValueType.POINTS);
+				}
+			} else {
+				numvalue = new Value(num, ValueType.SCALAR);
+			}
+			return numvalue;
+		} else {
+			return new Value(num, ValueType.SCALAR);
+		} 
+						
+		
+	}
+	
+	/*spec for evalExpression
+	mutates tokens
+	i haven't yet adjusted for the possibility that you never get the close paren - take that into account
+	*/
+	public Value evalExpression(ArrayList<Token> tokens){
+		//figure out how much of the array is this expression, and transfer to a new array
+		int parencount = 1;
+		ArrayList<Token> sublist = new ArrayList<Token>();
+		tokens.remove(0);
+		boolean closedparen = false;
+		while (parencount != 0 | closedparen == false){
+			Token x = tokens.get(0);
+			
+			//adjust our two values for checking when we are done finding the subexpression
+			//first adjust parencount
+			if (x.type==Type.OPENPAREN){
+				parencount++;
+			} else if (x.type==Type.CLOSEPAREN){
+				parencount--;
+			}
+			//now adjust our boolean to say if we are currently on a closed parenthesis
+			if (x.type==Type.CLOSEPAREN){
+				closedparen = true;
+			} else {
+				closedparen = false;
+			}
+			//break if we've finished the expression and remove the current token(closeparen)
+			if (parencount==0 && closedparen == true){
+				tokens.remove(0);
+				break;
+			}
+			//do what the while loop actually does - it transfers from tokens to sublist
+			sublist.add(x);
+			tokens.remove(0);
+		}
+		
+		//the while loop is completed 
+		//we now have separated the subexpression from our main tokens array
+		//so recurse on the subarray
+		return evaluate(sublist);
+	}
+	
+	
+	public Value convertScalar(Value value, String unit){
+		if (unit=="in"){
+			return new Value(value.value*PT_PER_IN, ValueType.INCHES);
+		} else if (unit=="pt"){
+			return new Value(value.value, ValueType.POINTS);
+		}
+		return new Value(value.value, ValueType.POINTS);	
+	}
+	
+	public Value convertNonScalar(Value value, String unit){
+		if (unit == "in"){
+			return new Value(value.value, ValueType.INCHES);
+		} else if (unit=="pt"){
+			return new Value(value.value, ValueType.POINTS);
+		}
+		return new Value(value.value, ValueType.POINTS);
 	}
 }
+
+
+/* Description of how evaluate works - write out then implement
+Start reading the tokens
+You start by expecting one of two possibilities:
+	1. NUMBER
+	2. OPENPARENTHESIS
+	If it isn't one of those two types - error.
+	If open parenthesis 
+		find the closed parenthesis that goes with it (by counting parenthesis tokens) - and call evaluate on that sub expression + EOF
+		that returns a Value. Then, treat that value as if it were a number/unit combination (with possibility of a unit shift following)
+	If a number -
+		treat number as a scalar
+
+Step 2
+	Expecting one of:
+	Units
+	Operation
+	EOF (or other EOF indicator before closed paren)
+	
+	If Units 
+		convert to the units - go to step 2 (but no possibility of units - max 1 is allowed)
+	If EOF/other end of expression (closed paren) - we're done - return the Value(value,type)
+	If operation
+		- we need something to operate by - save the operator and move onto step 3
+		
+Step 3
+	As in step 1 - we're looking for a number or an expression
+		If NUMBER 
+			- treat as scalar - go to step 4
+		IF Expression - eval expression
+			
+Step 4 
+	Check for Units - and convert as necessary
+	
+Step 5 
+	Expected EOF - evaluate the expression and return the value
+	
+*/
---
+++ ps3/src/calculator/ParserTest.java
@@ -0,0 +1,58 @@
+package calculator;
+
+public class ParserTest{
+
+    public static void main(String[] args){
+        testParser();
+    }
+    
+
+
+public static void testParser(){
+    //Test 1 "3+2.4"
+    System.out.println("Test 1: 3+2.4");
+    Lexer test1 = new Lexer("3+2.4");
+    Parser test01 = new Parser(test1);
+    Parser.Value value1 = test01.evaluate(test01.alltokens);
+    System.out.println(value1.value);
+    System.out.println(value1.type);
+
+        
+    //Test 2 "3in * 5.5in"
+    System.out.println("Test 2: 3in * 5.5in");
+    System.out.println("Expected answer: 16.5 INCHES");
+    System.out.println("Expected answer is problematic b/c we need to divide by 72 squared");
+    Parser test02 = new Parser(new Lexer("3in * 5.5in"));
+    Parser.Value value2 = test02.evaluate(test02.alltokens);
+    System.out.println(value2.value);
+    System.out.println(value2.type);
+    
+    
+            
+    //Test 3 "3in + 5.5in"
+    System.out.println("Test 3: 3in + 5.5in");
+    System.out.println("Expected answer: 612.0 INCHES");
+    Parser test03 = new Parser(new Lexer("3in + 5.5in"));
+    Parser.Value value3 = test03.evaluate(test03.alltokens);
+    System.out.println(value3.value);
+    System.out.println(value3.type);
+    
+    
+    //Test 4 "5 * (10in - 3in)"
+    System.out.println("Test 4: 5 * (10in - 3in)");
+    System.out.println("Expected answer 2520.0 INCHES");
+    Parser test04 = new Parser(new Lexer("5 * (10in - 3in)"));
+    Parser.Value value4 = test04.evaluate(test04.alltokens);
+    System.out.println(value4.value);
+    System.out.println(value4.type);
+    
+    //Test 5 "3 * (10pt + (10 / 2)pt)"
+    System.out.println("test 5: 3 * (10pt + (10 / 2)pt)");
+    System.out.println("expected answer 45 pt");
+    Parser test05 = new Parser(new Lexer("3 * (10pt + (10 / 2)pt)"));
+    Parser.Value value5 = test05.evaluate(test05.alltokens);
+    System.out.println(value5.value);
+    System.out.println(value5.type);
+
+}
+}
